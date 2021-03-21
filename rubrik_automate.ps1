84920d39f258c121f939c80f78776ffc332aa5e6
jeremycathey <64913779+jeremycathey@users.noreply.github.com>
2021-03-20 18:50:45 -0700

    Add files via upload

---
+++ rubrik_automate.ps1
@@ -0,0 +1,9 @@
+﻿$ScriptPath = 'G:\My Drive\Scripts\Powershell\Rubrik_Automate\'
+$FunctionPath = $ScriptPath + 'RA_Functions\RA_Functions.psm1'
+Import-Module $FunctionPath
+
+#Connect with Rubrik Cluster Using Username/Password (Basic Auth)
+$ConnectionInfo = RA_Connect_Creds
+
+#Present RubrikAutomate Menu for a menu driven system to LiveMount VMs and Databases via CSV Import
+RA_Menu -ConnectionInfo $ConnectionInfo -ScriptPath $ScriptPath
