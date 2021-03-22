b81ed71b5522ad4ed7c488403802f7fb68687125
jeremycathey <64913779+jeremycathey@users.noreply.github.com>
2021-03-21 20:39:13 -0700

    Delete RA_GetAPIAuth.psm1

--- RA_Functions/RA_GetAPIAuth.psm1
+++
@@ -1,5 +0,0 @@
-﻿Function RA_GetAPIAuth{
-    param($Creds)
-        $auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Creds.UserName + ':' + $Creds.GetNetworkCredential().Password))
-        return $auth
-    }
