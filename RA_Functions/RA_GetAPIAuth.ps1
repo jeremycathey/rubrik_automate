fa67d28a13a311816ccc32748d615d39b668d6f7
jeremycathey <64913779+jeremycathey@users.noreply.github.com>
2021-03-24 20:21:40 -0700

    Add files via upload

---
+++ RA_Functions/RA_GetAPIAuth.ps1
@@ -0,0 +1,5 @@
+﻿Function RA_GetAPIAuth{
+    param($Creds)
+        $auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Creds.UserName + ':' + $Creds.GetNetworkCredential().Password))
+        return $auth
+    }
